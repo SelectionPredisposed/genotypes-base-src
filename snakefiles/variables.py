b6bf4a9ccc3403d795aa6452f12c03e2f062f0dd
Øyvind Helgeland <helgeland.oyvind@gmail.com>
2018-05-24 19:13:08 +0200

    Commited changes before rewriting to use bcftools to annotate dot notation markers

--- snakefiles/variables.py
+++ snakefiles/variables.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# Config files containing hard coded variableus
+configfile: os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'snakefiles', 'harvest.json'))
+
 ### snakemake_workflows initialization ########################################
 libdir = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'lib'))
 bcftools = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'software/bcftools-1.7/bcftools'))
@@ -9,6 +12,7 @@ plinklocal = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'so
 
 ### workflow settings ##################################
 chrom = list(range(1,23))
+chromx = chrom + ['X']
 
 ### generate paths ###################################
 if not os.path.exists(config['output_base']):
